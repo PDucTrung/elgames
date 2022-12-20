49e0d3ba8514a174f81ec7e5cd943f1c9b5081ce
trung <trungezeal115@gmail.com>
2022-12-21 02:05:21 +0700

    dsd

--- pages/components/Layout.jsx
+++ pages/components/Layout.jsx
@@ -2,14 +2,21 @@ import React, { useEffect, useState } from "react";
 import BackToTop from "./back-to-top/BackToTop";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
-import { collection, getFirestore, getDocs, query } from "firebase/firestore";
+import {
+  collection,
+  getFirestore,
+  getDocs,
+  query,
+  doc,
+} from "firebase/firestore";
 import { app } from "../../lib/firebase";
 
 const Layout = (props) => {
+  const db = getFirestore(app);
   const [games, setGames] = useState([]);
 
   useEffect(() => {
-    const q = query(collection(getFirestore(app), "games"));
+    const q = query(collection(db, "games"));
 
     getDocs(q)
       .then((snapshot) => {
@@ -17,8 +24,27 @@ const Layout = (props) => {
           id: item.id,
           ...item.data(),
         }));
-
         setGames(games);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    const q = query(collection(db, "games"));
+
+    getDocs(q)
+      .then((snapshot) => {
+        const games = snapshot.docs.map((item) => ({
+          id: item.id,
+          ...item.data(),
+        }));
+        const game = games.splice(
+          games.findIndex((item) => item.id == 50),
+          1
+        )[0];
+        console.log(game);
       })
       .catch((err) => {
         console.error(err);
