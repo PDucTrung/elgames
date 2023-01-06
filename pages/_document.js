30537c99a9584847181758b53fd0f485d33107fd
trung <trungezeal115@gmail.com>
2023-01-06 17:07:24 +0700

    dd

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,8 @@ export default function Document() {
       <Head>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="games store by reactjs" />
         <link rel="icon" href="/img/logo-head.png" />
-        <meta property="og:image" content="/img/logo-head.png" />
       </Head>
       <body>
         <Main />
