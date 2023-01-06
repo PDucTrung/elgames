c79f0b2440275c5702b2c598af554ec39919cc26
trung <trungezeal115@gmail.com>
2023-01-06 17:02:00 +0700

    dsd

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="games store by reactjs" />
         <link rel="icon" href="/img/logo-head.png" />
+        <meta property="og:image" content="/img/logo-head.png" />
       </Head>
       <body>
         <Main />
